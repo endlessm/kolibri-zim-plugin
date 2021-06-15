a040eb800e2d6fb05118abd1e09a711163f294d6
Dylan McCall <dylan@endlessos.org>
2021-06-14 17:50:11 -0700

    ZimRendererIndex: Add a random article button

--- kolibri_zim_plugin/root_urls.py
+++ kolibri_zim_plugin/root_urls.py
@@ -2,6 +2,7 @@ from kolibri.dist.django.conf.urls import url
 
 from .views import ZimArticleView
 from .views import ZimIndexView
+from .views import ZimRandomArticleView
 from .views import ZimSearchView
 
 urlpatterns = [
@@ -14,6 +15,11 @@ urlpatterns = [
         r"^zim/content/(?P<zim_filename>[^/]+.zim)/(?P<zim_article_path>.+)$",
         ZimArticleView.as_view(),
         name="zim_article",
+    ),
+    url(
+        r"^zim/random/(?P<zim_filename>[^/]+.zim)$",
+        ZimRandomArticleView.as_view(),
+        name="zim_random_article",
     ),
     url(
         r"^zim/search/(?P<zim_filename>[^/]+.zim)$",
