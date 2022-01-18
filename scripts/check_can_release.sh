5ca3f9e4ffdbb58ea081e620e9b2093355dd03da
Dylan McCall <dylan@endlessos.org>
2022-01-17 16:40:18 -0800

    Fix incorrect repo URL in check_can_release.sh
    https://phabricator.endlessm.com/T32973

--- scripts/check_can_release.sh
+++ scripts/check_can_release.sh
@@ -3,14 +3,14 @@
 # Check that all is pushed so it's ready to release
 HEAD=$(git rev-parse --verify HEAD)
 TAG=$(git describe --tags)
-MASTER_HASH=$(git ls-remote https://github.com/endlessm/kolibri-explore-plugin.git master | cut -f1)
+MASTER_HASH=$(git ls-remote https://github.com/endlessm/kolibri-zim-plugin.git master | cut -f1)
 if [ $HEAD != $MASTER_HASH ]
 then
     echo "The repository is not pushed"
     exit 1
 fi
 
-TAG_HASH=$(git ls-remote https://github.com/endlessm/kolibri-explore-plugin.git $TAG^{} | cut -f1)
+TAG_HASH=$(git ls-remote https://github.com/endlessm/kolibri-zim-plugin.git $TAG^{} | cut -f1)
 if [[ -z $TAG_HASH || $TAG_HASH != $MASTER_HASH ]]
 then
     echo "The tag is not updated or it's not the latest commit in master"
